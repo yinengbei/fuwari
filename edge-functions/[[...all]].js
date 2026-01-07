efb1b20b3537876575add22d3f783cdfd5850149
依依Yiyi <yi_nengbei@163.com>
2026-01-07 22:39:52 +0800

    Update [[...all]].js

--- edge-functions/[[...all]].js
+++ edge-functions/[[...all]].js
@@ -1,8 +1,12 @@
-export default async function onRequest(context) {
-  const { request } = context;
-  const checkmode = request.headers.get("checkmode");
-  if (checkmode) {
+export default async function middleware(request) {
+  if (request.headers.get("checkmode")) {
     return new Response("", { status: 500 });
   }
+
+  const url = new URL(request.url);
+  if (url.pathname === "/") {
+    return new Response("", { status: 500 });
+  }
+
   return fetch(request);
 }
