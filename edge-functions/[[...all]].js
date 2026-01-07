3cd1c93e2c75195ccdefa67ac41ab4dd26c09efd
依依Yiyi <yi_nengbei@163.com>
2026-01-07 22:32:42 +0800

    防止itdog测速

---
+++ edge-functions/[[...all]].js
@@ -0,0 +1,8 @@
+export default async function onRequest(context) {
+  const { request } = context;
+  const checkmode = request.headers.get("checkmode");
+  if (checkmode) {
+    return new Response("", { status: 500 });
+  }
+  return fetch(request);
+}
